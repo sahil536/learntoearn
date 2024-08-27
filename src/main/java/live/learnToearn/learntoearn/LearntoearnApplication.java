1efe7e05acdd6474a087e9517334141d4d4b10ba
SAHIL <mdghufran57@gmail.com>
2024-08-27 23:54:04 +0800

    Changes for ui

--- src/main/java/live/learnToearn/learntoearn/LearntoearnApplication.java
+++ src/main/java/live/learnToearn/learntoearn/LearntoearnApplication.java
@@ -2,12 +2,19 @@ package live.learnToearn.learntoearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearntoearnApplication {
+public class LearntoearnApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearntoearnApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearntoearnApplication.class);
+    }
+
 
 }
